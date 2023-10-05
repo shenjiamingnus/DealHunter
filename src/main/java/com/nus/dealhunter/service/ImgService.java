953abf7d7bb0afe0d8d76a6a07c31ab351564529
shenjiamingnus <e1221616@u.nus.edu>
2023-10-05 22:38:32 +0800

    Implement upload img

--- src/main/java/com/nus/dealhunter/service/ImgService.java
+++ src/main/java/com/nus/dealhunter/service/ImgService.java
@@ -2,34 +2,38 @@ package com.nus.dealhunter.service;
 
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ImgService {
 
-  public static final String ACCESS_KEY = "tNlOb8NL6fAVb90yjgbf";
+  @Value("${minio.access_key}")
+  public String accessKey;
 
-  public static final String SECRET_KEY = "ieCXgFn3xAmMwCGff4hZQK1QjZW2DDsma6T4Spsu";
+  @Value("${minio.secret_key}")
+  public String secretKey;
 
-  public static final String BUCKET = "dealhunter";
+  @Value("${minio.bucket}")
+  public String bucket;
 
-  public static final String END_POINT = "http://68.183.176.202:9000";
+  @Value("${minio.end_point}")
+  public String endPoint;
 
   public String uploadImage(MultipartFile file) {
     String objectName = UUID.randomUUID().toString();
     try {
       InputStream fileInputStream = file.getInputStream();
       //1.创建minio链接客户端
-      MinioClient minioClient = MinioClient.builder().credentials(ACCESS_KEY, SECRET_KEY).endpoint(END_POINT).build();
+      MinioClient minioClient = MinioClient.builder().credentials(accessKey, secretKey).endpoint(endPoint).build();
       //2.上传
       PutObjectArgs putObjectArgs = PutObjectArgs.builder()
           .object(objectName+".png")
           .contentType("image/jpg")
-          .bucket(BUCKET)
+          .bucket(bucket)
           .stream(fileInputStream, fileInputStream.available(), -1)
           .build();
       minioClient.putObject(putObjectArgs);
@@ -38,7 +42,7 @@ public class ImgService {
       ex.printStackTrace();
       return null;
     }
-    return END_POINT + "/" + BUCKET + "/" + objectName + ".png";
+    return endPoint + "/" + bucket + "/" + objectName + ".png";
   }
 
 }
