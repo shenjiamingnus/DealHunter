eeefdfa07c751569dcfd54261cd77dffcf094ebd
jialiang <jialiang67@gmail.com>
2023-10-05 16:39:22 +0800

    add model

---
+++ src/main/java/com/nus/dealhunter/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.nus.dealhunter.controller;
+
+import com.nus.dealhunter.service.UserService;
+import com.nus.dealhunter.util.JwtTokenUtil;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Api("Product/")
+@RestController
+@RequestMapping("/api/product")
+
+public class ProductController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserService productService;
+
+    @Autowired
+    JwtTokenUtil jwtTokenUtil;
+
+
+
+
+}
---
+++ src/main/java/com/nus/dealhunter/model/Brand.java
@@ -0,0 +1,45 @@
+package com.nus.dealhunter.model;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+
+@Data
+@Entity
+@Table(name = "brands", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"brandname"})
+})
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String brandname;
+
+    private String discription;
+
+    @CreatedDate
+    private Instant createDate;
+
+    public Brand(String brandname) {
+        this.brandname = brandname;
+
+    }
+
+    public Brand() {}
+
+
+
+
+
+
+
+}
