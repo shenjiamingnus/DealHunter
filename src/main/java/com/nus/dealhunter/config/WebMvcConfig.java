ce9f4a7fa7802795cc0b33449a0792b97bfdcf3e
shenjiamingnus <e1221616@u.nus.edu>
2023-10-29 19:31:57 +0800

    re-imple the product controller

--- src/main/java/com/nus/dealhunter/config/WebMvcConfig.java
+++ src/main/java/com/nus/dealhunter/config/WebMvcConfig.java
@@ -15,7 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins(allowedOrigins)
+        .allowedOrigins("*")
         .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
         .allowedHeaders("*")
         .exposedHeaders("*");
