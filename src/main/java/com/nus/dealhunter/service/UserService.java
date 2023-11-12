aa39bcc491488ef75b4eb3a1fa003ae02f078147
shenjiamingnus <e1221616@u.nus.edu>
2023-11-12 22:41:02 +0800

    user Log in implementation

--- src/main/java/com/nus/dealhunter/service/UserService.java
+++ src/main/java/com/nus/dealhunter/service/UserService.java
@@ -63,6 +63,7 @@ public class UserService {
     user.setCreateDate(Instant.now());
     user.setEmail(signupRequest.getEmail());
     return userRepository.save(user);
+
   }
 
   public User createAdminUser(AdminCreateRequest adminCreateRequest) {
